b6c7bad8ed7ec9cad8923f2a0e717f65daa9eaa2
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-01-18 17:14:22 -0300

    finalizando desafio

--- app/tabuleiro/page.tsx
+++ app/tabuleiro/page.tsx
@@ -1,15 +1,26 @@
 import Casa from "../../components/Casa"
 
-function montarLinha() {
+function montarLinha(colunas: number) {
     var linha = [];
 
-    for (let index = 0; index < 10; index++)
-        linha.push(<Casa index={index} />);
+    let contador = 1
+    let cor = 'white'
+    for (let index = 0; index < 64; index++){
+        contador++
+        linha.push(<Casa index={index} cor={cor} />);
+        if(contador > colunas){
+            contador = 1
+            continue
+        }
+        cor = cor == 'white' ? 'black' : 'white'
+    }
 
     return linha;
 }
 
 export default function tabuleiro() {
+    const colunas = 8
+    const maxWidth = 100 * colunas
     return (
         <div style={{
             backgroundColor: 'gray',
@@ -19,7 +30,15 @@ export default function tabuleiro() {
             display: "flex",
             flexWrap: 'wrap'
         }}>
-            {montarLinha()}
+            <div style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                display: "flex",
+                flexWrap: 'wrap',
+                maxWidth: `${maxWidth}px`
+            }}>
+                {montarLinha(colunas)}
+            </div>
         </div>
     )
 }
--- components/Casa.jsx
+++ components/Casa.jsx
@@ -1,9 +1,11 @@
 export default function casa(props) {
     return (
-        <div style={{
-            height: '100px',
-            width: '100px',
-            backgroundColor: props.index % 2 === 0 ? 'white' : 'black'
-        }} ></div>
+        <>
+            <div style={{
+                height: '100px',
+                width: '100px',
+                backgroundColor: props.cor,
+            }}></div>
+        </>
     )
 }
